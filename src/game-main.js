0f1777bd51d4f80db2bd2eb6b6015554ababdd74
Florian <f_bron@hotmail.com>
2019-03-28 01:08:39 +0900

    Basic collision engine

--- src/game-main.js
+++ src/game-main.js
@@ -1,11 +1,62 @@
 import {startGame, vdp} from "../lib/vdp-lib";
+import {getMapBlock, setMapBlock} from './utils';
+
+const tileSize = 16;
+
+function collidesAt(x, y) {
+	const blockX = Math.floor(x / tileSize);
+	const blockY = Math.floor(y / tileSize);
+	// A wall block?
+	return getMapBlock('level1', blockX, blockY) === 38;
+}
 
 function *main() {
+	const input = vdp.input;
+
+	const mario = {
+		left: 0,
+		top: 0,
+		width: vdp.sprite('mario').tile(6).w,
+		height: vdp.sprite('mario').tile(6).h,
+		get right() { return this.left + this.width; },
+		get bottom() { return this.top + this.height; },
+	};
+	const moveSpeed = 1;
+
 	vdp.configBackdropColor('#084');
 
 	while (true) {
+		vdp.drawBackgroundTilemap('level1');
+		// We need to use the tile no 6 for the standing Mario
+		vdp.drawObject(vdp.sprite('mario').tile(6), mario.left, mario.top);
+
+		const movement = {
+			x: 0,
+			y: 0
+		};
+		if (input.isDown(input.Key.Left)) movement.x -= moveSpeed;
+		if (input.isDown(input.Key.Right)) movement.x += moveSpeed;
+		if (input.isDown(input.Key.Up)) movement.y -= moveSpeed;
+		if (input.isDown(input.Key.Down)) movement.y += moveSpeed;
+
+		mario.left += movement.x;
+		while (collidesAt(mario.right, mario.top) || collidesAt(mario.right, mario.bottom)) {
+			mario.left -= 1;
+		}
+		while (collidesAt(mario.left, mario.top) || collidesAt(mario.left, mario.bottom)) {
+			mario.left += 1;
+		}
+
+		mario.top += movement.y;
+		while (collidesAt(mario.left, mario.bottom) || collidesAt(mario.right, mario.bottom)) {
+			mario.top -= 1;
+		}
+		while (collidesAt(mario.left, mario.top) || collidesAt(mario.right, mario.top)) {
+			mario.top += 1;
+		}
+
 		yield;
 	}
 }
 
-startGame('#glCanvas', vdp => main());
+startGame('#glCanvas', vdp => main(vdp));
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
-import { vdp } from './game-main';
+import {startGame, vdp} from "../lib/vdp-lib";
 
-function readMapBlock(name, x, y) {
+
+export function getMapBlock(name, x, y) {
 	const mapData = vdp.readMap(vdp.map(name).offset(x, y, 1, 1));
 	return mapData.array[0];
 }
 
-function writeMapBlock(name, x, y, newValue) {
+export function setMapBlock(name, x, y, newValue) {
 	const mapAddress = vdp.map(name).offset(x, y, 1, 1);
 	const mapData = vdp.readMap(mapAddress);
 	mapData.array[0] = newValue;
