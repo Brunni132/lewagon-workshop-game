fc1a1b164405806b8f8d8953274d5227828a55a2
Florian <f_bron@hotmail.com>
2020-02-14 01:06:49 +0900

    Fixed get map block from float

--- src/utils.js
+++ src/utils.js
@@ -19,12 +19,13 @@ export function clamp(val, min, max) {
 }
 
 export function getMapBlock(name, x, y) {
-	const mapData = vdp.readMap(vdp.map(name).offset(x, y, 1, 1));
+	const mapAddress = vdp.map(name).offset(Math.floor(x), Math.floor(y), 1, 1);
+	const mapData = vdp.readMap(mapAddress);
 	return mapData.array[0];
 }
 
 export function setMapBlock(name, x, y, newValue) {
-	const mapAddress = vdp.map(name).offset(x, y, 1, 1);
+	const mapAddress = vdp.map(name).offset(Math.floor(x), Math.floor(y), 1, 1);
 	const mapData = vdp.readMap(mapAddress);
 	mapData.array[0] = newValue;
 	vdp.writeMap(mapAddress, mapData);
